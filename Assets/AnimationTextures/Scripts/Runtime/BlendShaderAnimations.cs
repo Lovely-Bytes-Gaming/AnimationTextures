d93336317f745bfe95fa3d01a774d603a5462190
NicholasGraetzLBG <nicholas.graetz@lovelybytesgaming.com>
2025-04-04 13:48:20 +0200

    added example script for blending two clips from the same animation texture

---
+++ Assets/AnimationTextures/Scripts/Runtime/BlendShaderAnimations.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace LovelyBytes.AnimationTextures
+{
+    /// <summary>
+    /// Example script for blending between 2 shader animations that are encoded in the same texture
+    /// </summary>
+    public class BlendShaderAnimations : MonoBehaviour
+    {
+        [Min(0)] 
+        public int 
+            ClipIndexA = 0, 
+            ClipIndexB = 0;
+        
+        [SerializeField] 
+        private MultiClipInfoReference _multiClipInfoRef;
+
+        [SerializeField] 
+        private float _speed;
+
+        [SerializeField, Range(0f, 1f)] 
+        private float _blend;
+
+        [SerializeField] 
+        private Renderer _renderer;
+
+        private float _time0, _time1;
+
+        private static readonly int _clipTimeAProp = Shader.PropertyToID("_ClipTimeA"); 
+        private static readonly int _clipTimeBProp = Shader.PropertyToID("_ClipTimeB");
+        private static readonly int _blendProp = Shader.PropertyToID("_Blend");
+        
+        private void Update()
+        {
+            _time0 += Time.deltaTime * _speed;
+            _time1 += Time.deltaTime * _speed;
+            
+            ClipInfo a = _multiClipInfoRef.Value.Entries[ClipIndexA];
+            ClipInfo b = _multiClipInfoRef.Value.Entries[ClipIndexB];
+
+            _time0 %= 1f;
+            _time1 %= 1f;
+
+            if (_blend < Mathf.Epsilon)
+                _time1 = 0f;
+            
+            if (_blend > 1f - Mathf.Epsilon)
+                _time0 = 0f;
+            
+            _renderer.material.SetFloat(_clipTimeAProp, Mathf.Lerp(a.NormalizedStartTime, a.NormalizedEndTime, _time0));
+            _renderer.material.SetFloat(_clipTimeBProp, Mathf.Lerp(b.NormalizedStartTime, b.NormalizedEndTime, _time1));
+            _renderer.material.SetFloat(_blendProp, _blend);
+        }
+    }
+}
