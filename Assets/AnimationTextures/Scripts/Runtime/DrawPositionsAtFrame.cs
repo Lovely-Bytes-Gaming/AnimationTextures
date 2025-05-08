9875659adc9cdc6e42d6768c3e1a4bc293e59968
NicholasGraetzLBG <nicholas.graetz@lovelybytesgaming.com>
2025-05-08 09:59:50 +0200

    changed draw position at frame to work during runtime

--- Assets/AnimationTextures/Scripts/Runtime/DrawPositionsAtFrame.cs
+++ Assets/AnimationTextures/Scripts/Runtime/DrawPositionsAtFrame.cs
@@ -13,19 +13,25 @@ namespace LovelyBytes.AnimationTextures
         [SerializeField] 
         private BoundingBox _boundingBox;
 
-        [SerializeField]
-        private Mesh _mesh;
-        private Mesh _meshCopy;
-
-        private Mesh _currentMesh;
+        [SerializeField] 
+        private MeshFilter _meshFilter;
         
+        private Mesh _currentMesh;
+
+        private void Update()
+        {
+            SetVertices();
+        }
+
         private void SetVertices()
         {
             var verts = new Vector3[_texture.width];
 
+            Mesh mesh = _meshFilter.mesh;
+            
             for (int i = 0; i < _texture.width; ++i)
             {
-                int x = Mathf.RoundToInt(_meshCopy.uv2[i].x * _meshCopy.vertexCount - 0.5f);
+                int x = Mathf.RoundToInt(mesh.uv3[i].x * mesh.vertexCount - 0.5f);
                 int y = _frame;
                 
                 Color c = _texture.GetPixel(x, y);
@@ -33,32 +39,13 @@ namespace LovelyBytes.AnimationTextures
                 v = _boundingBox.ToAbsolutePosition(v);
                 verts[i] = v;
             }
-            
-            _meshCopy.SetVertices(verts);
-        }
-
-        private void OnValidate()
-        {
-            if (!_texture || !_mesh || !_boundingBox) 
-                return;
-            
-            if (_currentMesh != _mesh)
-            {
-                _currentMesh = _mesh;
-                    
-                if (_meshCopy)
-                    DestroyImmediate(_meshCopy);
-                    
-                _meshCopy = Instantiate(_mesh);
-            }
-                
-            SetVertices();
+            mesh.SetVertices(verts);
         }
 
         private void OnDrawGizmos()
         {
-            if (_meshCopy)
-                Gizmos.DrawMesh(_meshCopy, transform.position);
+            //if (_meshCopy)
+            //    Gizmos.DrawMesh(_meshCopy, transform.position);
         }
     }
 }
